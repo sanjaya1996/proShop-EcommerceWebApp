77d1b990718c5e0f10514a70a3799c054f1cd240
Sanzay Dahal <sanzaydahal12345@gmail.com>
2020-10-15 21:40:15 +1100

    before implementing backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import Header from './components/Header';
+import HomeScreen from './screens/HomeScreen';
+import Footer from './components/Footer';
+import ProductScreen from './screens/ProductScreen';
 
 const App = () => {
   return (
-    <>
-      <h1>Welcome To ProShop</h1>
-    </>
+    <Router>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <Route path='/' component={HomeScreen} exact />
+          <Route path='/product/:id' component={ProductScreen} />
+        </Container>
+      </main>
+      <Footer />
+    </Router>
   );
 };
 
