78e04dfadb04ad69f018d22a60666a8e0db1515f
Sanzay Dahal <sanzaydahal12345@gmail.com>
2020-10-19 10:33:35 +1100

    Fetch Products from database and custom error handling

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,9 @@ import express from 'express';
 import dotenv from 'dotenv';
 import colors from 'colors';
 
-import products from './data/products.js';
 import connectDB from './config/db.js';
+import productRoutes from './routes/productRoutes.js';
+import { notFound, erroHandler } from './middleware/errorMiddleware.js';
 
 dotenv.config();
 
@@ -15,14 +16,11 @@ app.get('/', (req, res) => {
   res.send('API is running');
 });
 
-app.get('/api/products', (req, res) => {
-  res.json(products);
-});
+app.use('/api/products', productRoutes);
 
-app.get('/api/products/:id', (req, res) => {
-  const product = products.find((prod) => prod._id === req.params.id);
-  res.json(product);
-});
+app.use(notFound);
+
+app.use(erroHandler);
 
 const PORT = process.env.PORT || 5000;
 
