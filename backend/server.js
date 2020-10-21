a5c98f2bf3cb2243b090b5a9fe675c900f51f2a7
Sanzay Dahal <sanzaydahal12345@gmail.com>
2020-10-21 12:35:39 +1100

    Using Controllers, User Authentication EndPoint & Generate JWT

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import colors from 'colors';
 
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 import { notFound, erroHandler } from './middleware/errorMiddleware.js';
 
 dotenv.config();
@@ -12,9 +13,13 @@ connectDB();
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('API is running');
 });
+
+app.use('/api/users', userRoutes);
 
 app.use('/api/products', productRoutes);
 
