b0a648c5b16b268f33dd4ec0569416d8d123ce9d
Sanzay Dahal <sanzaydahal12345@gmail.com>
2020-11-04 09:04:03 +1100

    Fix OrderListScreen Typo

--- frontend/src/screens/OrderListScreen.js
+++ frontend/src/screens/OrderListScreen.js
@@ -60,7 +60,7 @@ const OrderListScreen = ({ history }) => {
                 </td>
                 <td>
                   {order.isDelivered ? (
-                    order.deliverdAt.substring(0, 10)
+                    order.deliveredAt.substring(0, 10)
                   ) : (
                     <i className='fas fa-times' style={{ color: 'red' }}></i>
                   )}
